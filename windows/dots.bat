c2ea330ba2b86c2674d24f11ad8c255cf44394d7
mirachan010 <mirachan010@gmail.com>
2020-08-18 20:34:32 +0900

    userless batfile

--- windows/dots.bat
+++ windows/dots.bat
@@ -1,4 +1,4 @@
-mklink c:\Users\mira\AppData\Local\nvim\init.vim c:\Users\mira\dotfiles\windows\init.vim
-mklink c:\Users\mira\.vimrc c:\Users\mira\dotfiles\windows\init.vim
-mklink /d "C:\Users\mira\AppData\Local\coc\ultisnips" "C:\Users\mira\dotfiles\common\vim\ultisnips"
-mklink C:\Users\mira\AppData\Local\nvim\coc-settings.json "C:\Users\mira\dotfiles\windows\coc-settings.json"
+mkdir %APPDATA%\..\Local\nvim\
+mklink %APPDATA%\..\..\.vimrc %APPDATA%\..\..\dotfiles\windows\vimsetting.vim
+mklink %APPDATA%\..\Local\nvim\init.vim %APPDATA%\..\..\dotfiles\windows\vimsetting.vim
+mklink %APPDATA%\..\Local\nvim\coc-settings.json %APPDATA%\..\..\dotfiles\windows\coc-settings.json
