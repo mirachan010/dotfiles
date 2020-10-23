7f00dbe42a89b317505666230828efbc2e123165
mirachan010 <mirachan010@gmail.com>
2020-10-24 07:57:09 +0900

    make link for minimum setting

--- windows/dots.bat
+++ windows/dots.bat
@@ -1,4 +1,5 @@
 mkdir %APPDATA%\..\Local\nvim\
 mklink %APPDATA%\..\..\.vimrc %APPDATA%\..\..\dotfiles\common\vim\vimsetting.vim
+mklink %APPDATA%\..\..\small.vim %APPDATA%\..\..\dotfiles\common\vim\small.vim
 mklink %APPDATA%\..\Local\nvim\init.vim %APPDATA%\..\..\dotfiles\common\vim\vimsetting.vim
 mklink %APPDATA%\..\Local\nvim\coc-settings.json %APPDATA%\..\..\dotfiles\windows\coc-settings.json
