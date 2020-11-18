90319d4dadda2926c8236f500b98a2dee36bd88e
mirachan010 <mirachan010@gmail.com>
2020-11-18 13:47:13 +0900

    for nvim-qt

--- windows/dots.bat
+++ windows/dots.bat
@@ -2,4 +2,5 @@ mkdir %APPDATA%\..\Local\nvim\
 mklink %APPDATA%\..\..\.vimrc %APPDATA%\..\..\dotfiles\common\vim\vimsetting.vim
 mklink %APPDATA%\..\..\small.vim %APPDATA%\..\..\dotfiles\common\vim\small.vim
 mklink %APPDATA%\..\Local\nvim\init.vim %APPDATA%\..\..\dotfiles\common\vim\vimsetting.vim
+mklink %APPDATA%\..\Local\nvim\ginit.vim %APPDATA%\..\..\dotfiles\windows\ginit.vim
 mklink %APPDATA%\..\Local\nvim\coc-settings.json %APPDATA%\..\..\dotfiles\windows\coc-settings.json
