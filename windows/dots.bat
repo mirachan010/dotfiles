a87838495634dfe8fdd566455e986dd5af6dbe55
mirachan010 <mirachan010@gmail.com>
2022-11-15 00:39:50 +0900

    make for Powershell link

--- windows/dots.bat
+++ windows/dots.bat
@@ -7,3 +7,7 @@ mklink /d %APPDATA%\..\Local\nvim\lua %APPDATA%\..\..\dotfiles\common\neovim\lua
 mklink %APPDATA%\..\Local\nvim\ginit.vim %APPDATA%\..\..\dotfiles\windows\ginit.vim
 mklink %APPDATA%\..\Local\nvim\coc-settings.json %APPDATA%\..\..\dotfiles\windows\coc-settings.json
 mklink %APPDATA%\..\..\.gitconfig %APPDATA%\..\..\dotfiles\common\.gitconfig
+mkdir %APPDATA%\..\..\Documents\WindowsPowerShell\
+mklink %APPDATA%\..\..\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 %APPDATA%\..\..\dotfiles\windows\Microsoft.PowerShell_profile.ps1
+mkdir %APPDATA%\..\..\Documents\PowerShell\
+mklink %APPDATA%\..\..\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 %APPDATA%\..\..\dotfiles\windows\Microsoft.PowerShell_profile.ps1
