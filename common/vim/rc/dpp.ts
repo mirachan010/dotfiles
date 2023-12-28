9d84ad65cb0675ef7a68a2a10d5a891e888b0a2e
mirachan010 <mirachan010@gmail.com>
2023-12-29 07:34:50 +0900

    set dpp

---
+++ common/vim/rc/dpp.ts
@@ -0,0 +1,180 @@
+import {
+  BaseConfig,
+  ConfigReturn,
+  ContextBuilder,
+  Dpp,
+  Plugin,
+} from "https://deno.land/x/dpp_vim@v0.0.9/types.ts";
+import { Denops, fn } from "https://deno.land/x/dpp_vim@v0.0.9/deps.ts";
+
+type Toml = {
+  hooks_file?: string;
+  ftplugins?: Record<string, string>;
+  plugins?: Plugin[];
+};
+
+type LazyMakeStateResult = {
+  plugins: Plugin[];
+  stateLines: string[];
+};
+
+export class Config extends BaseConfig {
+  override async config(args: {
+    denops: Denops;
+    contextBuilder: ContextBuilder;
+    basePath: string;
+    dpp: Dpp;
+  }): Promise<ConfigReturn> {
+    const hasNvim = args.denops.meta.host === "nvim";
+    const hasWindows = await fn.has(args.denops, "win32");
+
+    const inlineVimrcs = [
+      "$BASE_DIR/01main_rc.vim",
+      "$BASE_DIR/02keymappings_rc.vim",
+      "$BASE_DIR/03plugin_rc.vim",
+      "$BASE_DIR/04filetype_rc.vim",
+    ];
+    if (hasNvim) {
+      inlineVimrcs.push("$BASE_DIR/neovim.rc.vim");
+    } else if (await fn.has(args.denops, "gui_running")) {
+      inlineVimrcs.push("$BASE_DIR/gui.rc.vim");
+    }
+    if (hasWindows) {
+      inlineVimrcs.push("$BASE_DIR/windows_rc.vim");
+    } else {
+      inlineVimrcs.push("$BASE_DIR/unix.rc.vim");
+    }
+
+    args.contextBuilder.setGlobal({
+      inlineVimrcs,
+      extParams: {
+        installer: {
+          checkDiff: true,
+          logFilePath: "~/.cache/dpp/installer-log.txt",
+          githubAPIToken: Deno.env.get("GITHUB_API_TOKEN"),
+        },
+      },
+      protocols: ["git"],
+    });
+
+    const [context, options] = await args.contextBuilder.get(args.denops);
+
+    // Load toml plugins
+    const tomls: Toml[] = [];
+    for (
+      const tomlFile of [
+        "$BASE_DIR/toml/00dpp.toml",
+      ]
+    ) {
+      const toml = await args.dpp.extAction(
+        args.denops,
+        context,
+        options,
+        "toml",
+        "load",
+        {
+          path: tomlFile,
+          options: {
+            lazy: false,
+          },
+        },
+      ) as Toml | undefined;
+
+      if (toml) {
+        tomls.push(toml);
+      }
+    }
+    for (
+      const tomlFile of [
+        "$BASE_DIR/toml/01lazy.toml",
+        hasNvim ? "$BASE_DIR/toml/neovim.toml" : "$BASE_DIR/toml/vim.toml",
+      ]
+    ) {
+      const toml = await args.dpp.extAction(
+        args.denops,
+        context,
+        options,
+        "toml",
+        "load",
+        {
+          path: tomlFile,
+          options: {
+            lazy: true,
+          },
+        },
+      ) as Toml | undefined;
+
+      if (toml) {
+        tomls.push(toml);
+      }
+    }
+
+    // Merge toml results
+    const recordPlugins: Record<string, Plugin> = {};
+    const ftplugins: Record<string, string> = {};
+    const hooksFiles: string[] = [];
+    for (const toml of tomls) {
+      for (const plugin of toml.plugins ?? []) {
+        recordPlugins[plugin.name] = plugin;
+      }
+
+      if (toml.ftplugins) {
+        for (const filetype of Object.keys(toml.ftplugins)) {
+          if (ftplugins[filetype]) {
+            ftplugins[filetype] += `\n${toml.ftplugins[filetype]}`;
+          } else {
+            ftplugins[filetype] = toml.ftplugins[filetype];
+          }
+        }
+      }
+
+      if (toml.hooks_file) {
+        hooksFiles.push(toml.hooks_file);
+      }
+    }
+
+
+    const packSpecPlugins = await args.dpp.extAction(
+      args.denops,
+      context,
+      options,
+      "packspec",
+      "load",
+      {
+        basePath: args.basePath,
+        plugins: Object.values(recordPlugins),
+      },
+    ) as Plugin[] | undefined;
+    if (packSpecPlugins) {
+      for (const plugin of packSpecPlugins) {
+        if (plugin.name in recordPlugins) {
+          recordPlugins[plugin.name] = Object.assign(
+            recordPlugins[plugin.name],
+            plugin,
+          );
+        } else {
+          recordPlugins[plugin.name] = plugin;
+        }
+      }
+    }
+    //console.log(packSpecPlugins);
+
+    const lazyResult = await args.dpp.extAction(
+      args.denops,
+      context,
+      options,
+      "lazy",
+      "makeState",
+      {
+        plugins: Object.values(recordPlugins),
+      },
+    ) as LazyMakeStateResult | undefined;
+
+    return {
+      ftplugins,
+      hooksFiles,
+      plugins: lazyResult?.plugins ?? [],
+      stateLines: lazyResult?.stateLines ?? [],
+    };
+  }
+}
